80d545b373c9bc4a0bf89cfa8c8b141617d7b8a5
Ronnie Voss <ronvoss@rvoss.com>
2016-07-10 12:58:54 -0500

    Configure contact form redirection

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -21,17 +21,3 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
-
-var message = "";
-
-$("#sendMessage").on("click", function() {
-    message = $("#contactform").serialize();
-    $.ajax({
-        url: "//formspree.io/ronvoss@rvoss.com", 
-        method: "POST",
-        data: {message: message},
-        dataType: "json"
-    });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    return false;
-});
